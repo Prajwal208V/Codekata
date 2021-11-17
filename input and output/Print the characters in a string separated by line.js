0ef3d6920385cdffb10ba2380b231c3dd1668e2b
Prajwal V <92531202+Prajwal208V@users.noreply.github.com>
2021-11-17 23:42:53 +0530

    Create Print the characters in a string separated by line.js

---
+++ input and output/Print the characters in a string separated by line.js
@@ -0,0 +1,26 @@
+// Write a code to get the input in the given format and print the output in the given format.
+
+// Input Description:
+// A single line contains a string.
+
+// Output Description:
+// Print the characters in a string separated by line.
+
+// Sample Input :
+// guvigeek
+// Sample Output :
+// g
+// u
+// v
+// i
+// g
+// e
+// e
+// k
+var str='guvigeek';
+var n=str.length;
+for(var i=0;i<n;i++){
+    console.log(str[i]);
+}
+
+
