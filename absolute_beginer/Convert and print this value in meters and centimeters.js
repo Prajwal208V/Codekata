f9f2fb8d15034411e496f0ef649b0bd0f8fd475d
Prajwal V <92531202+Prajwal208V@users.noreply.github.com>
2021-11-17 23:54:05 +0530

    Create Convert and print this value in meters and centimeters.js

---
+++ absolute_beginer/Convert and print this value in meters and centimeters.js
@@ -0,0 +1,19 @@
+// You are given a number A in Kilometers. Convert this into B: Meters and C: Centi-Metres.
+
+// Input Description:
+// A number "A" representing some distance in kilometer is provided to you as the input.
+
+// Output Description:
+// Convert and print this value in meters and centimeters.
+
+// Sample Input :
+// 2
+// Sample Output :
+// 2000
+// 200000
+
+var a=2;
+var meters=a*1000;
+var centiMeters=a*100000;
+console.log(meters);
+console.log(centiMeters);
