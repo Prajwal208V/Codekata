8968a89cf0c7f08babb4cff7c562d5d35ce941b7
Prajwal V <92531202+Prajwal208V@users.noreply.github.com>
2021-11-18 00:21:16 +0530

    Create cube of the number.js

---
+++ absolute_beginer/cube of the number.js
@@ -0,0 +1,15 @@
+// You are given with a number "N", find its cube.
+
+// Input Description:
+// A positive integer is provided.
+
+// Output Description:
+// Find the cube of the number.
+
+// Sample Input :
+// 2
+// Sample Output :
+// 8
+
+var a=2;
+console.log(a**3);
