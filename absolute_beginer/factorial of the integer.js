f684d101b92252ee084bf3e6844fb16d82c46d0b
Prajwal V <92531202+Prajwal208V@users.noreply.github.com>
2021-11-18 00:17:01 +0530

    Create factorial of the integer.js

---
+++ absolute_beginer/factorial of the integer.js
@@ -0,0 +1,19 @@
+// You are provided with a number, "N". Find its factorial.
+
+// Input Description:
+// A positive integer is provided as an input.
+
+// Output Description:
+// Print the factorial of the integer.
+
+// Sample Input :
+// 2
+// Sample Output :
+// 2
+
+var a=10;
+var temp=1;
+for(var i=1;i<=a;i++){
+    temp *=i;
+}
+console.log(temp);
