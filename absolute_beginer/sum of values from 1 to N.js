2e11764f37dd7961156a537266826cf01b24382a
Prajwal V <92531202+Prajwal208V@users.noreply.github.com>
2021-11-18 01:40:11 +0530

    Create sum of values from 1 to N.js

---
+++ absolute_beginer/sum of values from 1 to N.js
@@ -0,0 +1,16 @@
+/*Write a code to get an integer N and print the sum of  values from 1 to N.
+
+Input Description:
+A single line contains an integer N.
+
+Output Description:
+Print the sum of values from 1 to N.
+
+Sample Input :
+10
+Sample Output :
+55
+*/
+
+var n=10;
+console.log((n*(n+1))/2);
