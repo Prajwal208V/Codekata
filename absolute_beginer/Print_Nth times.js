b701232bedd349dccec9357c7b6684470d5a4456
Prajwal V <92531202+Prajwal208V@users.noreply.github.com>
2021-11-18 01:25:29 +0530

    Create Print_Nth times.js

---
+++ absolute_beginer/Print_Nth times.js
@@ -0,0 +1,22 @@
+/*Write a code to get 2 integers A and N. Print the integer A, N times in separate line.
+
+Input Description:
+First line contains an integer A. Second line contains an Integer N.
+
+Output Description:
+Print the integer A, N times in a separate line.
+
+Sample Input :
+2 3
+Sample Output :
+2
+2
+2
+*/
+
+var a=2;
+var b=10;
+
+for(var i=1; i <= b; i++){
+    console.log(a);
+}
