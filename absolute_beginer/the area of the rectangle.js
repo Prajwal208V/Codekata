5fe031994d9f916449a7baae17b772ae218a09a5
Prajwal V <92531202+Prajwal208V@users.noreply.github.com>
2021-11-18 00:19:21 +0530

    Create the area of the rectangle.js

---
+++ absolute_beginer/the area of the rectangle.js
@@ -0,0 +1,19 @@
+// You are given A = Length of a rectangle & B = breadth of a rectangle. Find its area “C”.
+
+// (A and B are natural numbers)
+
+// Input Description:
+// The inputs are two natural numbers representing the length and the breadth of a rectangle.
+
+// Output Description:
+// Find the area of the rectangle formed by the provided input. Round off the answer to the first decimal place if required.
+
+// Sample Input :
+// 2
+// 3
+// Sample Output :
+// 6
+
+var a=10;
+var b=3;
+console.log(a*b);
