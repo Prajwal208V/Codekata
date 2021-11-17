7efc042953516ca45ef0adf42305635c193a6784
Prajwal V <92531202+Prajwal208V@users.noreply.github.com>
2021-11-18 00:34:01 +0530

    Create Circumference of the circle.js

---
+++ absolute_beginer/Circumference of the circle.js
@@ -0,0 +1,21 @@
+// 8
+// Share
+// You are provided with the radius of a circle "A". Find the length of its circumference.
+
+// Note: In case the output is coming in decimal, roundoff to 2nd decimal place. In case the input is a negative number, print "Error".
+
+// Input Description:
+// The Radius of a circle is provided as the input of the program.
+
+// Output Description:
+// Calculate and print the Circumference of the circle corresponding to the input radius up to two decimal places.
+
+// Sample Input :
+// 2
+// Sample Output :
+// 12.57
+
+var A=2;
+var result=(2*A*Math.PI);
+result=result.toFixed(2);
+console.log(result);
