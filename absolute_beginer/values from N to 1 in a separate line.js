9297836fae8897d5f30374d10eaf77cf96e76f19
Prajwal V <92531202+Prajwal208V@users.noreply.github.com>
2021-11-18 00:35:57 +0530

    Create values from N to 1 in a separate line.js

---
+++ absolute_beginer/values from N to 1 in a separate line.js
@@ -0,0 +1,26 @@
+// Write a code to get an integer N and print the values from N to 1.
+
+// Input Description:
+// A single line contains an integer N.
+
+// Output Description:
+// Print the values from N to 1 in a separate line.
+
+// Sample Input :
+// 10
+// Sample Output :
+// 10
+// 9
+// 8
+// 7
+// 6
+// 5
+// 4
+// 3
+// 2
+// 1
+
+var n=10;
+for(var i=n;i>=1;i--){
+    console.log(i);
+}
