3212978e4149d3da0355d36103cafee8c63633be
Prajwal V <92531202+Prajwal208V@users.noreply.github.com>
2021-11-18 16:15:30 +0530

    product is a perfect square
    Print 'yes' if their product is a perfect square else print 'no'.

---
+++ mathematics/perfect square.js
@@ -0,0 +1,14 @@
+/*
+Given 2 numbers N,M. Print 'yes' if their product is a perfect square else print 'no'.
+Sample Testcase :
+INPUT
+5 5
+OUTPUT
+yes
+*/
+
+var some=[5,5];
+if(some[0]==some[1]){
+    console.log("yes");
+}
+else console.log("no");
